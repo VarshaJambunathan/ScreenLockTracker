3afc57e125d7b98f69438ed167389ea004dc1520
VarshaJambunathan <jvarsha621@gmail.com>
2019-01-15 19:59:04 +0530

    Adapter to display screen lock items

--- app/src/main/java/com/varsha/screenlocktimetracker/Adapters/ScreenLockAdapter.java
+++ app/src/main/java/com/varsha/screenlocktimetracker/Adapters/ScreenLockAdapter.java
@@ -1,8 +1,60 @@
 package com.varsha.screenlocktimetracker.Adapters;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.varsha.screenlocktimetracker.R;
+
+import java.util.ArrayList;
+
 /**
  * Created by Varsha on 15-01-2019.
  */
 
-public class ScreenLockAdapter {
+public class ScreenLockAdapter extends RecyclerView.Adapter<ScreenLockAdapter.ViewHolder> {
+    private final Context context;
+    private ArrayList<String> mItemsList;
+
+    public ScreenLockAdapter(Context context, ArrayList<String> mItemsList) {
+        this.context = context;
+        this.mItemsList = mItemsList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row_layout, parent, false);
+
+        ScreenLockAdapter.ViewHolder vh = new ScreenLockAdapter.ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mLabel.setText(mItemsList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItemsList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView mLabel;
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            mLabel = (TextView) itemView.findViewById(R.id.label);
+
+        }
+    }
 }
--- app/src/main/java/com/varsha/screenlocktimetracker/MainActivity.java
+++ app/src/main/java/com/varsha/screenlocktimetracker/MainActivity.java
@@ -2,12 +2,32 @@ package com.varsha.screenlocktimetracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.varsha.screenlocktimetracker.Adapters.ScreenLockAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView mItemsView;
+    private RecyclerView.Adapter mItemsAdapter;
+    private RecyclerView.LayoutManager mItemsLayoutManager;
+    private ArrayList<String> mItemsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mItemsView = (RecyclerView) findViewById(R.id.items_view);
+        mItemsView.setHasFixedSize(true);
+        mItemsLayoutManager = new LinearLayoutManager(this);
+        mItemsView.setLayoutManager(mItemsLayoutManager);
+
+        mItemsList = new ArrayList<>();
+
+        mItemsAdapter = new ScreenLockAdapter(getApplicationContext(), mItemsList);
+        mItemsView.setAdapter(mItemsAdapter);
     }
 }
